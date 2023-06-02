acf109816f50da2896b96d86764d5f650f34b487
johnrhimawan <johnr.himawan@gmail.com>
2023-06-02 17:18:31 +0800

    add social media buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
-import { motion } from 'framer-motion';
-
 import { styles } from '../styles';
+import { socials } from '../constants';
 
 const Hero = () => {
   return (
@@ -12,17 +11,31 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-              Hi, I'm <span className='text-[#915EFF]'>John</span>
-            </h1>
-            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              Computer Science Student at the National University of Singapore (NUS) <br className='sm:block hidden' />
-              <br/>
-              Interested in software development, data analysis, and entrepreneurship
-            </p>     
+            Hi, I'm <span className='text-[#915EFF]'>John</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 mb-10 text-white-100`}>
+            Computer Science Student at the National University of Singapore (NUS) <br className='sm:block hidden' />
+            <br/>
+            Interested in software development, data analysis, and entrepreneurship
+          </p>
+        </div>
+      </div>
+      <div className='absolute left-1/2 transform -translate-x-1/2 bottom-20 sm:bottom-20'>
+        <div className='mt-5 flex flex-col gap-5 justify-center'>
+          {socials.map((social) => (
+            <a
+              href={social.url}
+              target='_blank'
+              className="bg-tertiary rounded-[20px] py-5 px-40   flex items-center"
+            >
+              <img src={social.icon} alt={social.alt} className="w-15 h-10 mr-2" />
+              <span>{social.title}</span>
+            </a>
+          ))}
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
